5ac4948b1b338308d6ad536a9f08da24abdea1df
dolpen <dolpen@dolpen.net>
2016-05-26 01:58:04 +0900

    add weapon component

--- src/main.js
+++ src/main.js
@@ -1,25 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import weapons from "./weapons.json"
+import weapons from "./weapons.json";
 
-// raw js
-console.log(weapons);
+class Weapon extends React.Component {
+    render() {
+        return (
+            <li>
+                {this.props.data.name} ( {this.props.data.sub} / {this.props.data.special} )
+            </li>
+        );
+    }
+}
 
 class App extends React.Component {
-    constructor(...args){
+    constructor(...args) {
         super(...args);
         this.state = {
-            filterQuery : ""
+            filterQuery: ""
         };
     }
+
+    onChangeFilterQuery(event) {
+        this.setState({
+            filterQuery: event.target.value
+        });
+    }
+
     render() {
         return (
             <div className="weapons">
-                <input />
+                <input onChange={this.onChangeFilterQuery.bind(this)}/>
                 <ul>
                     {
-                        weapons.map((weapon, index) => {
-                            return (<li key={index}>{weapon.name}</li>);
+                        weapons.filter(weapon => {
+                            return weapon.name.indexOf(this.state.filterQuery) > -1
+                                || weapon.sub.indexOf(this.state.filterQuery) > -1
+                                || weapon.special.indexOf(this.state.filterQuery) > -1;
+                        }).map((weapon, index) => {
+                            return (<Weapon key={index} data={weapon}/>);
                         })
                     }
                 </ul>
